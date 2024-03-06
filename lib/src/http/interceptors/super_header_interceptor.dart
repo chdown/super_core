5012713596ac4d84677c5106f84ae8634aba996d
chenghao <473726199@qq.com>
2024-03-06 16:35:07 +0800

    ⚡️perf: super_header_interceptor支持异步

--- lib/src/http/interceptors/super_header_interceptor.dart
+++ lib/src/http/interceptors/super_header_interceptor.dart
@@ -1,17 +1,14 @@
 import 'package:dio/dio.dart';
-import 'package:super_core/src/config/super_net_config.dart';
-import 'package:super_core/src/http/app_net_error.dart';
-import 'package:super_core/src/http/super_http.dart';
 
 class SuperHeaderInterceptor extends Interceptor {
-
-  Map<String,String> Function() headerParams;
+  Future<Map<String, String>> Function() headerParams;
 
   SuperHeaderInterceptor(this.headerParams);
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
-    headerParams().forEach((key, value) {
+    Map<String, String> headers = await headerParams();
+    headers.forEach((key, value) {
       options.headers[key] = value;
     });
     super.onRequest(options, handler);

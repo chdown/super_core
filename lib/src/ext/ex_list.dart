e7326612d8c0c3178f4e935f2a3d75499c277789
chenghao <473726199@qq.com>
2024-09-21 10:20:19 +0800

    ✨feat: 扩展和修改字段

--- lib/src/ext/ex_list.dart
+++ lib/src/ext/ex_list.dart
@@ -11,12 +11,22 @@ extension ListExt<T> on List<T>? {
   /// 是否不为null或空
   bool get isNotEmptyOrNull => ObjUtil.isNotEmpty(this);
 
-  bool firstWhereOrNullBool(bool Function(T element) test) {
+  /// 查找元素是否存在
+  bool findWhere(bool Function(T element) test) {
     if (this == null) return false;
     for (var element in this!) {
       if (test(element)) return true;
     }
     return false;
+  }
+
+  /// 查找元素
+  T? findWhereOrNull(bool Function(T element) test) {
+    if (this == null) return null;
+    for (var element in this!) {
+      if (test(element)) return element;
+    }
+    return null;
   }
 
   /// 返回空数组

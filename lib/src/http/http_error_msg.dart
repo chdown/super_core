8778c987cd1424b4b4354595859a6e2f21b6574c
ch <473726199@qq.com>
2025-03-25 19:48:29 +0800

    refactor(http): 优化错误消息处理方式并更新部分错误提示文案
    - 将错误消息方法改为函数属性，提高灵活性和可测试性
    - 更新部分错误提示文案，使其更具用户友好性
    - 统一错误消息格式，提升用户体验

--- lib/src/http/http_error_msg.dart
+++ lib/src/http/http_error_msg.dart
@@ -3,25 +3,25 @@
 /// @description 错误消息
 ///
 class HttpErrorMsg {
-  static String errorDataMsg() => "数据错误，请稍后再试！";
+  static String Function() errorDataMsg = () => "数据错误，请稍后再试！";
 
-  static String errorNetUnConnectionMsg() => "无法连接服务器，请检查您的网络环境！";
+  static String Function() errorNetUnConnectionMsg = () => "无法连接服务器，请检查您的网络环境！";
 
-  static String sendTimeoutMsg() => "请求服务器超时，请稍后再试！";
+  static String Function() sendTimeoutMsg = () => "请求服务器超时，请稍后再试！";
 
-  static String connectionTimeoutMsg() => "连接服务器超时，请稍后再试！";
+  static String Function() connectionTimeoutMsg = () => "连接服务器超时，请稍后再试！";
 
-  static String connectionErrorMsg() => "连接服务器异常，请稍后再试！";
+  static String Function() connectionErrorMsg = () => "连接服务器异常，请稍后再试！";
 
-  static String badCertificateMsg() => "请求证书异常，请稍后再试！";
+  static String Function() badCertificateMsg = () => "请求证书异常，请稍后再试！";
 
-  static String cancelMsg() => "请求被异常取消，请稍后再试！";
+  static String Function() cancelMsg = () => "请求被异常取消，请稍后再试！";
 
-  static String receiveTimeoutMsg() => "响应超时，请稍后再试！";
+  static String Function() receiveTimeoutMsg = () => "响应超时，请稍后再试！";
 
-  static String unknownMsg() => "未知异常，请稍后再试！";
+  static String Function() unknownMsg = () => "未知异常，请检查网络后再试！";
 
-  static String badResponseMsg() => "服务器异常，请稍后重试！";
+  static String Function() badResponseMsg = () => "服务器异常，请稍后重试！";
 
-  static String requestMsg() => "网络请求异常，请稍后重试！";
+  static String Function() requestMsg = () => "网络请求异常，请稍后重试！";
 }

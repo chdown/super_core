92018ff72729919178bfc10eaf23aaaf33ce349d
ch <473726199@qq.com>
2025-05-14 11:16:54 +0800

    refactor: 移除未使用的导入
    - 删除了未使用的 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart' 导入
    - 优化代码结构，提高代码可读性和维护性

--- lib/src/ext/widget/ex_gesture.dart
+++ lib/src/ext/widget/ex_gesture.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:super_core/src/utils/click_throttler_utils.dart';

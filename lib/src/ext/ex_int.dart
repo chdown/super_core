a1bab098d687996e27f95603c9a4af74a4e22559
ch <473726199@qq.com>
2025-10-17 13:48:58 +0800

    docs(ex_int): 更新扩展注释描述
    - 将布尔方法注释更正为整型方法
    - 调整布尔值转换逻辑说明
    - 移除多余空行保持代码整洁

--- lib/src/ext/ex_int.dart
+++ lib/src/ext/ex_int.dart
@@ -2,10 +2,9 @@ import 'package:super_core/super_core.dart';
 
 /// @author : ch
 /// @date 2024-02-18 09:34:57
-/// @description bool方法
+/// @description int方法
 ///
 extension ExtensionInt on int? {
-
   /// 是否为null或空
   bool get isEmptyOrNull => ObjUtil.isEmpty(this);
 
@@ -18,7 +17,7 @@ extension ExtensionInt on int? {
   /// 不为空时返回Obj
   int notEmptyToNew(int obj) => isNotEmptyOrNull ? obj : this!;
 
-  /// string形式的bool值
+  /// int形式的bool值
   bool get boolValue => this != null && this == 1;
 
   /// 取反value
